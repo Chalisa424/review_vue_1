e09759ff41c31346d767ea97259e814b02878402
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-11-21 13:57:00 +0700

    เพิ่ม computed ไว้เก็บค่า/catch

--- new-app/src/App.vue
+++ new-app/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const firstname = ref('')
 const lastname = ref('')
+
+const fullname = computed(() =>{
+  return `${firstname.value} ${lastname.value}`
+})
 </script>
 <template>
-  <div>{{ firstname }} {{ lastname }}</div>
+  <div>{{ fullname }}</div>
   First Name<input type="text" name="firstname" v-model="firstname">
   Last Name<input type="text" name="lastname" v-model="lastname">
  
