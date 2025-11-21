510c766d045d0c6272ff593352bd36ecab37b5da
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-11-21 11:47:17 +0700

    input

--- new-app/src/App.vue
+++ new-app/src/App.vue
@@ -2,47 +2,15 @@
   <div>
     <div>
       <div>First Name</div>
-      <input type="text" name="firstname" v-model="formData.firstname" />
+      <input type="text" name="firstname" />
     </div>
     <div>
       <div>Last Name</div>
-      <input type="text" name="lastname" v-model="formData.lastname" />
-    </div>
-    <div>
-      <div>Genger</div>
-      <div v-for="gender in genderList">
-        <input type="radio" name="gender" :value="gender" v-model="formData.gender" />
-        {{ gender }}
-      </div>
-    </div>
-    <div>
-      <div>Interest</div>
-      <div v-for="interest in interestList">
-        <input type="checkbox" name="interest" :value="interest" v-model="formData.interest"/>
-        {{ interest }}
-      </div>
+      <input type="text" name="lastname" />
     </div>
     <div>
       <div>Description</div>
-      <textarea name="description" v-model="formData.description" ></textarea>
+      <textarea name="description"></textarea>
     </div>
-    <button @click="submitForm()">Submit</button>
   </div>
 </template>
-
-<script setup>
-import { reactive } from 'vue';
-
-const genderList = ["ผู้ชาย", "ผู้หญิง", "ไม่ระบุเพศ"];
-const interestList = ["เกม", "กีฬา", "การเมือง", "ฟังเพลง"];
-
-const formData = reactive({
-  interest:[]
-})
-
-const submitForm = () =>{
-  console.log('submit')
-  console.log('formdata', formData)
-  console.log('interest',formData.interest.join())
-}
-</script>
