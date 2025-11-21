3765dafcd9a94cfe12611b10198aae5089ac1597
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-11-21 14:19:19 +0700

    เพิ่ม computed

--- new-app/src/App.vue
+++ new-app/src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-const firstname = ref('')
-const lastname = ref('')
+const priceUSD = ref(1)
+const exChangeRate = ref(35)
 
-const fullname = computed(() =>{
-  return `${firstname.value} ${lastname.value}`
+const priceTHB = computed(() =>{
+  return priceUSD.value * exChangeRate.value
 })
 </script>
 <template>
-  <div>{{ fullname }}</div>
-  First Name<input type="text" name="firstname" v-model="firstname">
-  Last Name<input type="text" name="lastname" v-model="lastname">
+  <div>USD{{ priceUSD }} THB{{ priceTHB }}</div>
+  <div>THB<input type="text" v-model="priceTHB "></div>
+  <div>USD<input type="text"  v-model="priceUSD"></div>
  
 </template>
